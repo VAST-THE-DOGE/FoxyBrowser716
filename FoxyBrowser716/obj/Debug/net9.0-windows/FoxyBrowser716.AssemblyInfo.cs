da8953918efe5400086019c1b00690b74564e405
William Herbert <Vastspacewill@gmail.com>
2024-12-21 11:47:50 -0600

    GOT THE RESIZING TO WORK WITH A CUSTOM BORDER!!!

--- FoxyBrowser716/obj/Debug/net9.0-windows/FoxyBrowser716.AssemblyInfo.cs
+++ FoxyBrowser716/obj/Debug/net9.0-windows/FoxyBrowser716.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VAST THE DOGE + FoxyGuy716")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+72fab3cef73dd0bba5c0c10199736ec7c267163e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+3f090f6b7803b277b598df5ccd6a11d119d31ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoxyBrowser716")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoxyBrowser716")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1")]
