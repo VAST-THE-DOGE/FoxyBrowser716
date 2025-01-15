ac87a3fb0639f27f11618a461b06a2c96594db3e
William Herbert <Vastspacewill@gmail.com>
2025-01-14 20:18:52 -0600

    fixed the search widget text not resizing correctly

---
+++ FoxyBrowser716/HomeWidgets/ActualHeightToFontSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FoxyBrowser716.Converters
+{
+    public class ActualHeightToFontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double actualHeight)
+            {
+                return actualHeight * 0.5;
+            }
+            return 12.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
