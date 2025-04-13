5f59f0b6e975d3025fca5910ce512c1a6a5abe98
William Herbert <Vastspacewill@gmail.com>
2025-04-12 20:30:57 -0500

    started work on the particle system

---
+++ FoxyBrowser716/HomeWidgets/RougeLiteClasses.cs
@@ -0,0 +1,51 @@
+using System.Drawing;
+using System.Numerics;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FoxyBrowser716.HomeWidgets;
+
+public static class RougeLiteParticles
+{
+	public class Explosion
+	{
+		private List<BoxVectorParticle> particles = [];
+		
+		//TODO
+		
+	}
+	
+	public class BoxVectorParticle
+	{
+		public Vector2 Velocity;
+		public Vector2 Acceleration;
+		public Line Box;
+
+		public void Move()
+		{
+			Box.X1 += Velocity.X;
+			Box.X2 += Velocity.X;
+			Box.Y1 += Velocity.Y;
+			Box.Y2 += Velocity.Y;
+			
+			Velocity.X += Acceleration.X;
+			Velocity.Y += Acceleration.Y;
+		}
+
+		public BoxVectorParticle(Vector2 velocity, Vector2 acceleration, Point start, int size, Brush brush)
+		{
+			Velocity = velocity;
+			Acceleration = acceleration;
+			Box = new Line
+			{
+				X1 = start.X,
+				X2 = start.X,
+				Y1 = start.Y,
+				Y2 = start.Y + size,
+
+				StrokeThickness = size,
+				Stroke = brush,
+			};
+		}
+	}
+}
--- FoxyBrowser716/HomeWidgets/RougeliteWidget.xaml.cs
+++ FoxyBrowser716/HomeWidgets/RougeliteWidget.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Numerics;
+using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using FoxyBrowser716.HomeWidgets.WidgetSettings;
+using Point = System.Drawing.Point;
 
 namespace FoxyBrowser716.HomeWidgets;
 
@@ -20,7 +22,7 @@ public partial class RougeWidget : IWidget
 	public override Task Initialize(TabManager manager, Dictionary<string, IWidgetSetting>? settings)
 	{
 		base.Initialize(manager, settings);
-		var timer = new System.Timers.Timer(30);
+		var timer = new System.Timers.Timer(50);
 		timer.Elapsed += Tick;
 		timer.AutoReset = true;
 		timer.Enabled = true;
@@ -28,11 +30,19 @@ public partial class RougeWidget : IWidget
 		return Task.CompletedTask;
 	}
 
+	private List<RougeLiteParticles.BoxVectorParticle> particles = [];
 	private void Tick(object? sender, ElapsedEventArgs elapsedEventArgs)
 	{
 		Dispatcher.Invoke(() =>
 		{
-			
+			var particle = new RougeLiteParticles.BoxVectorParticle(new Vector2(0,5), new Vector2(2,0), new Point(10,10), 5, Brushes.Aqua);
+			particles.Add(particle);
+			canvas.Children.Add(particle.Box);
+
+			foreach (var p in particles)
+			{
+				p.Move();
+			}
 		});
 	}
 }
