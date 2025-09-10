2d9e171aa66d8d681f8fde51fb5874344f8e30c6
VAST-THE-DOGE <145150405+VAST-THE-DOGE@users.noreply.github.com>
2025-09-10 12:26:07 -0500

    list of settings as comment to get an idea of what is all needed.

---
+++ FoxyBrowser716-WinUI/DataObjects/Settings/BrowserSettings.cs
@@ -0,0 +1,41 @@
+﻿namespace FoxyBrowser716_WinUI.DataObjects.Settings;
+
+public partial class BrowserSettings : ObservableObject
+{
+    public List<ISetting> GetSettingControls()
+    {
+        return [];
+    }
+    
+    //TODO: look more into this
+    //TODO: webview2 and everything needs an ApplySettings function that will call at creation and non change of settings.
+    /* Necessary Settings:
+     *
+     * Simple:
+     * - performance for webview2
+     * - performance for my UI
+     * - other webview2 specific settings
+     * - ai assistant settings
+     * - tracking prevention
+     * default zoom
+     * color theme (dark/light mode)
+     * startup mode: none, restore, config
+     * restore browser on close?
+     * download location
+     * show download menu on download start
+     * sleeping tabs and performance settings
+     * reset to defaults
+     * default search engine, last used or specific one
+     * default browser
+     * 
+     * Complex:
+     * Theme pick and editing
+     * extension management
+     * instance management
+     * error viewer/reporter/log-exporter
+     * startup config
+     * cookies and permission management for websites
+     * 
+     */
+    //TODO: need default static setting to use if none.
+}
